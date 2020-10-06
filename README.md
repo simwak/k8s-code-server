3f0ca7de68953a59de2094ed31034dfc5e041a88
Simon Wakenhut <simon.wakenhut@telekom.de>
2020-10-06 17:59:17 +0200

    Add kube config configuration

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Optimized Code Server image for Kubernetes
+# VS Coder Server on Kubernetes
 This is a optimized Code Server image for Kubernetes which is used in the [Calavera Project](https://github.com/simwak/calavera).
+
+Be aware, that only your home directory `/home/coder/` will be persistent!  
+`kubectl` is configured for defined namespace (`$NAMESPACE`).
--- init.sh
+++ init.sh
@@ -14,4 +14,11 @@ fi
 # Delete tmp home directory
 rm -rf /home/codertmp
 
+# Configure Kube Config
+kubectl config set-cluster local --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+kubectl config set-credentials service-account
+kubectl config set-credentials service-account --token=$(cat "/var/run/secrets/kubernetes.io/serviceaccount/token")
+kubectl config set-context local --user=service-account --namespace=$NAMESPACE
+kubectl config use-context local
+
 dumb-init fixuid -q /usr/bin/code-server --bind-addr 0.0.0.0:8080 .
